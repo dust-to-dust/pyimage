3629f6e4bdef95bbd3fdb822ee4b9d8924bc31e9
尘 <123@qq.com>
2022-03-16 17:33:00 +0800

    修改路径

--- pyimage/__init__.py
+++ pyimage/__init__.py
@@ -1,10 +1,10 @@
 
-from lib.downloader import Downloader, save_file
-from lib.threadpool import ThreadPool
-from lib.search_ori import search_ori_native
-from lib.shutil_ import copytree
-from lib.queue_ import QueueFIFO
-from lib.parse_html import *
-from lib.file_operate import *
+from .downloader import Downloader, save_file
+from .threadpool import ThreadPool
+from .search_ori import search_ori_native
+from .shutil_ import copytree
+from .queue_ import QueueFIFO
+from .parse_html import *
+from .file_operate import *
 
 __version__ = '2022-3-16'
