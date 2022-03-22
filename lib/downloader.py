e197d8638d0c2a7d9fa61f91d451371124dab641
尘 <123@qq.com>
2022-03-22 17:35:43 +0800

    修改默认格式为avif，以dhash代替截取url作为文件名

--- lib/downloader.py
+++ lib/downloader.py
@@ -1,19 +1,19 @@
 # Coding: UTF-8
 # Author: 尘
-# Datetime: 2021/8/3 
-# 使用说明: 实例化Downloader然后调用.download
-import logging
+# Datetime: 2021/8/3
+# instructions: 依赖： pillow_heif  pillow-avif-plugin
+# 图片浏览器 https://interversehq.com/qview/download/
+# 使用说明: 实例化Downloader然后调用.download即可
+
 import signal
-
-import PIL
-
 import lib
 import requests
+import warnings
 from lib.threadpool import ThreadPool
 import os
-from PIL import Image
 from io import BytesIO
 import json
+from PIL import Image, UnidentifiedImageError
 
 
 def save_file(fileurl, filepath):
@@ -35,14 +35,25 @@ def save_file(fileurl, filepath):
 
 
 class Downloader:
-    def __init__(self, save_to, max_workers=8, set_monitor=True, debug=False):
+    def __init__(self, save_to, max_workers=8, set_monitor=True, debug=False, img_format='avif', quantity=95):
         self.debug = debug  # 开启后打印错误信息
         self.max_workers = max_workers
         self.save_to = save_to
         self.success_count = 0
         self.task_count = 0
         self.exist_count = 0
+        self.img_format = img_format
+        self.quantity = quantity  # 保存图片的质量，默认95，
         self.close = self.shutdown
+
+        if img_format == 'avif':
+            try:
+                import pillow_avif  # 开启avif支持
+            except ImportError:
+                self.img_format = 'jpeg'
+                warnings.warn('未安装 pillow-avif-plugin包，不能保存为AVIF格式')
+        elif img_format == 'jpg':
+            self.img_format = 'jpeg'
 
         with open('下载记录.json', 'r+', encoding='utf-8') as f:
             self.record = json.load(f)
@@ -50,7 +61,7 @@ class Downloader:
         if set_monitor:
             self.pool.set_monitor()  # 启动线程池监视(进度条)
 
-        signal.signal(signal.SIGINT, self.shutdown_now)
+        signal.signal(signal.SIGINT, self.shutdown_now)  # 监听CTRL+C事件
 
         # 检查文件目录
         try:
@@ -97,48 +108,42 @@ class Downloader:
             url = 'https:' + url
         return url.split(sep='?')[0].split(sep='@')[0]
 
-    @staticmethod
-    def get_id(url: str):
-        """截取url末尾至多30位作为id"""
-        id_ = url.split('/')[-1]
-        if len(id_) > 30:
-            return id_[len(id_) - 30:]
-        else:
-            return id_
-
     def save_img(self, fileurl):
         """
         优先存为jpg格式，如有透明通道存png
         """
-        id_ = self.get_id(fileurl)
         fileurl = self.url_standardize(fileurl)
-        try:
-            # 在索引块record[id_[0]]查找是否下载过，如已下载函数返回(id_[0]是索引)
-            if id_ in self.record[id_[0]]:
-                self.exist_count += 1
-                return
-        except KeyError:
-            pass
 
         s = requests.Session()
         s.trust_env = False  # 使用VPN时必须加这句话!!!否则check_hostname requires server_hostname
         head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/92.0.4515.107 Safari/537.36'}
-        res = s.get(fileurl, headers=head)      # 下载
+        res = s.get(fileurl, headers=head)  # 下载
         # PIL不能直接读返回数据，需要将Bytes转IO流
         try:
             im: Image.Image = Image.open(BytesIO(res.content))
             if im.width == 0:
-                raise PIL.UnidentifiedImageError
-        except PIL.UnidentifiedImageError:
+                raise UnidentifiedImageError
+        except UnidentifiedImageError:
             # 下载失败/非图片
             raise Exception('下载失败或者非图片url')
+
+        dhash = im.dhash()
+        filepath = os.path.join(self.save_to, dhash)
+
         try:
-            im.save(os.path.join(self.save_to, lib.del_suffix(id_) + '.jpg'), 'jpeg')
+            # 在索引块record[dhash[:2]]查找是否下载过，如已下载函数返回(dhash[:2]是索引)
+            if dhash in self.record[dhash[:2]]:
+                self.exist_count += 1
+                return
+        except KeyError:
+            pass
+        try:
+            im.save(filepath + f'.{self.img_format}', format=self.img_format)
         except OSError:
-            im.save(os.path.join(self.save_to, lib.del_suffix(id_) + '.png'), 'png')
+            im.save(filepath + '.png', format='png')
         # 根据索引写入下载记录到变量（分块索引）
-        self.record.setdefault(id_[0], []).append(id_)
+        self.record.setdefault(dhash[:2], []).append(dhash)
         self.success_count += 1
         return
 
